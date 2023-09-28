79cadc118758c268f61e492f91b698042acc133e
Sneha Patil <sneha.patil@gyansys.com>
2023-09-28 16:55:08 +0530

    initial commit for School Management

---
+++ SchoolManagementCrud/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SchoolManagement.Models;
+
+namespace SchoolManagement.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Student> StudentTab { get; set; }
+    }
+}
